3028b2cb64ffef054b4b5761167bcb1cd08172e4
Zaland Khan <zaland@bluenose.cs.dal.ca>
2015-04-07 22:35:44 -0300

    updated test.c

--- a5/test.c
+++ a5/test.c
@@ -5,25 +5,44 @@
 #include "memory.h"
 
 
-/* Main function that runs the functions defined in memory.c. */
+/* Main function that runs the functions defined in memory.c. Test cases are
+   defined in the main function. */
 
 int main(int argc, char **argv)
 {
 	int return_value = 0;
-	void *p;
+	void *p, *q, *r, *s;
 	
+	
+	/* Start the memory and then start getting memory of various sizes. Some
+	   values should be smaller than 0 and larger than the allocated space. 
+	   Store some of them in pointers for releasing_memory function. */
+	   
 	start_memory(50); 
-	get_memory(10);
+	p = get_memory(10);
 	get_memory(4);
 	get_memory(2);
-	get_memory(2);
+	q = get_memory(2);
 	get_memory(24);
-	p = get_memory(10);
+	r = get_memory(10);
+	
+	
+	/* Print the list that has already been created before releasing memory.
+	   After releasing, print the list again to see the change. Also test
+	   removing one that is NULL. */
+	   
 	print(0);
-	print(1);
 	release_memory(p);
-	print(2);
-	print(1);
+	print(0);
+	release_memory(q);
+	print(0);
+	release_memory(r);
+	print(0);
+	release_memory(s);
+	
+	
+	/* Finally run the end_memory function to clean up the memory. */
+	
 	end_memory();
 	
 	return return_value;
